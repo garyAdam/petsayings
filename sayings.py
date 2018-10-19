87c5d03dfd0fad74981b4510f9eb5939ad9c9f46
garyAdam <rapture047@gmail.com>
2018-10-19 19:40:02 +0200

    langdetect

--- sayings.py
+++ sayings.py
@@ -1,16 +1,22 @@
 import sys
 import os
+import brainfuck
+from langdetect import detect
+def logo():
+    brainfuck.execute("brainfuckname.txt")
+
 def readfile(filename):
     f = open(filename,"r")
     lines=f.readlines()
     f.close()
-    return lines
-    
+    return lines  
 
 def filterlines(lines):
-    templines=[line for line in lines if len(line)>2]
-    templines2=[line for line in templines if "'" not in line and "(" not in line and "," in line]
-    filtered_lines=[templines2[i] for i in range(0,len(templines2),2)]
+    
+    lines=[line for line in lines if len(line)>2]
+    '''templines2=[line for line in templines if "'" not in line and "(" not in line and "," in line]
+    filtered_lines=[templines2[i] for i in range(0,len(templines2),2)]'''
+    filtered_lines=[line for line in lines if detect(line)=="hu" and "," in line]
     return filtered_lines
 
 
@@ -19,12 +25,9 @@ def getsaying(lines):
 
 
 
-
-
-
-
 def main():
     os.system("clear")
+    logo()
     print(filterlines(readfile("sayings.txt")))
 
 if __name__ == '__main__':
